129c081198d8c2d352a546ad62cf0fe8b124809b
urokuta <takuro.mizobe@gmail.com>
2014-01-07 04:19:58 +0900

    update ohmyzshcustomsh

--- oh-my-zsh-custom.sh
+++ oh-my-zsh-custom.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -x
 
-rm -fr ~/dotfiles/.oh-my-zsh/custom
-ln -s ~/dotfiles/.oh-my-zsh-customs/custom ~/dotfiles/.oh-my-zsh/custom
+OMZ_DIR=~/dotfiles/.oh-my-zsh/custom
+CUSTOM_DIR=~/dotfiles/.oh-my-zsh-customs/custom
+for item in `ls $CUSTOM_DIR`
+do
+  ln -s $CUSTOM_DIR/$item $OMZ_DIR/$item
+done
+
