03f2191dd79ba897caa031cbd5d34ba34448fa00
urokuta <takuro.mizobe@gmail.com>
2015-08-08 06:59:59 +0900

    add percol.d

---
+++ .percol.d/rc.py
@@ -0,0 +1,10 @@
+# Run command file for percol
+
+##########################################c
+percol.import_keymap({
+    "C-g" : lambda percol: percol.cancel(),
+    "C-j" : lambda percol: percol.command.select_next(),
+    "C-k" : lambda percol: percol.command.select_previous(),
+    "C-f" : lambda percol: percol.command.select_next_page(),
+    "C-b" : lambda percol: percol.command.select_previous_page(),
+})
