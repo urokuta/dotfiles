4c6c4f43cfdbc9f5f5d1c06a1e3587331462c960
urokuta <takuro.mizobe@gmail.com>
2018-09-10 16:41:19 +0900

    update dotsetup

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-for dotfile in .gitignore .gitconfig .zprofile .zshrc .vimrc .vim .oh-my-zsh .my.cnf .tmux.conf .percol.d
+for dotfile in .gitignore .gitconfig .zprofile .zshrc .vimrc .vim .oh-my-zsh .my.cnf .tmux.conf .percol.d .config
 do
   rm -fr ~/$dotfile
   ln -s $PWD/$dotfile ~/$dotfile
